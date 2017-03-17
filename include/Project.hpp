6ddb874433e8f0df16d9e2dd9bf692ddc650497f
Alejandro Linarez Rangel <alinarezrangel@gmail.com>
2017-03-16 19:41:27 -0500

    Documented Project classes

--- include/Project.hpp
+++ include/Project.hpp
@@ -35,6 +35,9 @@
 
 namespace GIDE
 {
+	/**
+	* @brief Contains the metadata of any project.
+	*/
 	struct ProjectMetadata
 	{
 		Glib::ustring project_name;
@@ -46,34 +49,144 @@ namespace GIDE
 		Glib::ustring build_system;
 	};
 
+	/**
+	* @brief The project class manages a GIDE project.
+	*/
 	class Project : public Glib::Object
 	{
 		public:
+			/**
+			* @brief Creates a new invalid void project.
+			*/
 			Project(void);
+
+			/**
+			* @brief Creates a project that points to the specified path.
+			*
+			* @param path_to_folder Path to the folder that contains the project.
+			*/
 			Project(const Glib::ustring& path_to_folder);
+
+			/**
+			* @brief Creates a project as a copy of another.
+			*/
 			Project(const Project& copy);
+
+			/**
+			* @brief Creates a project and moves all data from another.
+			*/
 			Project(Project&& move);
+
+			/**
+			* @brief Destroyes and frees the project.
+			*/
 			virtual ~Project(void);
 
+			/**
+			* @brief Gets if the project is valid.
+			*
+			* @return true if the project is valid, false otherwise.
+			*/
 			bool is_valid(void);
+
+			/**
+			* @brief Const version of is_valid.
+			*/
 			bool is_valid(void) const;
+
+			/**
+			* @brief Creates the project.
+			*
+			* Calling this will close any opened project and if fails will leave
+			* the project object in an undefined state in which any operation it's
+			* undefined.
+			*
+			* @param path_to_folder Path to the folder.
+			* @param tplt Template of the new project.
+			* @param meta Project metadata.
+			*
+			* @return true if the project was created successfully, false otherwise.
+			*/
 			bool create(
 				const Glib::ustring& path_to_folder,
 				ProjectTemplate& tplt,
 				const ProjectMetadata& meta
 			);
+
+			/**
+			* @brief Loads a project.
+			*
+			* Loads the project from the specified folder, which should contain a
+			* file named GIDE_PROJECT_FILENAME.
+			*
+			* @param path_to_folder Path to the existing project.
+			*
+			* @return true if the project was successfully readed, false otherwise.
+			*/
 			bool read_from(const Glib::ustring& path_to_folder);
+
+			/**
+			* @brief Validates a project file in the specified location.
+			*
+			* @param filename Path to the project file.
+			*
+			* @return true if the file is valid and false otherwise.
+			*/
 			bool validate_project_file(const Glib::ustring& filename);
+
+			/**
+			* @brief const version of validate_project_file
+			*/
 			bool validate_project_file(const Glib::ustring& filename) const;
 
+			/**
+			* @brief Gets the root folder of the project.
+			*
+			* @return RefPtr to the root folder.
+			*/
 			Glib::RefPtr<ProjectFolder> get_main_folder(void);
+
+			/**
+			* @brief Gets the GIDE_FILENAME_SRC folder.
+			*
+			* @return RefPtr to the source directory (if exists).
+			*/
 			Glib::RefPtr<ProjectFolder> get_source_folder(void);
+
+			/**
+			* @brief Gets the GIDE_FILENAME_INCLUDE folder.
+			*
+			* @return RefPtr to the include directory (if exists).
+			*/
 			Glib::RefPtr<ProjectFolder> get_include_folder(void);
+
+			/**
+			* @brief Gets the GIDE_FILENAME_UI folder.
+			*
+			* @return RefPtr to the UI Glade directory (if exists).
+			*/
 			Glib::RefPtr<ProjectFolder> get_ui_folder(void);
+
+			/**
+			* @brief Gets the GIDE_FILENAME_DOCS folder.
+			*
+			* @return RefPtr to the docs directory (if exists).
+			*/
 			Glib::RefPtr<ProjectFolder> get_docs_folder(void);
 
 		protected:
+			/**
+			* @brief Reads the project metadata from a keyfile.
+			*
+			* @param file KeyFile to read.
+			*/
 			void read_project_metadata(Glib::KeyFile& file);
+
+			/**
+			* @brief Saves the project metadata to a keyfile.
+			*
+			* @param file KeyFile in which save the current metadata.
+			*/
 			void save_project_metadata(Glib::KeyFile& file);
 
 		private:
--- include/ProjectTemplate.hpp
+++ include/ProjectTemplate.hpp
@@ -34,22 +34,79 @@
 
 namespace GIDE
 {
+	/**
+	* @brief A project template contains all structure and files for a project.
+	*
+	* The project template class should create and fills the files for the
+	* project, it's recommended to have a "template dir" from which the files
+	* will be copied.
+	*/
 	class ProjectTemplate : public Glib::Object
 	{
 		public:
+			/**
+			* @brief Creates the project template.
+			*/
 			ProjectTemplate(void);
+
 			ProjectTemplate(const ProjectTemplate&) = delete;
 			ProjectTemplate(ProjectTemplate&&) = delete;
+
+			/**
+			* @brief Destroyes the project template.
+			*/
 			virtual ~ProjectTemplate(void);
 
+			/**
+			* @brief Creates the project.
+			*
+			* This method should create and fills the files in a template-specific
+			* structure. It's recommended to the project to have the following
+			* folders on the root folder: A GIDE_FILENAME_SRC and
+			* GIDE_FILENAME_INCLUDE for headers and sources (on languages that
+			* have that separation) a GIDE_FILENAME_UI folder with the Glade files
+			* and a GIDE_FILENAME_DOCS folder that the user can fill with it's
+			* own documentation.
+			*
+			* @param folder Root folder in which the project should be created.
+			*/
 			virtual void create_template(Glib::RefPtr<ProjectFolder> folder);
 
+			/**
+			* @brief Gets the name of the template.
+			*
+			* @return The name of the template
+			*/
 			Glib::ustring get_name(void);
+
+			/**
+			* @brief Gets the description of the template.
+			*
+			* @return The description of the template
+			*/
 			Glib::ustring get_description(void);
 
 		protected:
+			/**
+			* @brief Creates the project template.
+			*
+			* This constructor is protected, but the ProjectTemplate subclasses
+			* should use it.
+			*
+			* @param name Name of the template.
+			* @param desc Description of the template.
+			*/
 			ProjectTemplate(const Glib::ustring& name, const Glib::ustring& desc);
 
+			/**
+			* @brief Creates and fills a file.
+			*
+			* Creates the specified file and fills it with the specified content.
+			*
+			* @param folder Folder in which the file will be created.
+			* @param filename The name of the new file.
+			* @param filecontents The contents of the file.
+			*/
 			void fill_file(
 				Glib::RefPtr<ProjectFolder> folder,
 				const Glib::ustring& filename,
@@ -61,8 +118,16 @@ namespace GIDE
 			Glib::ustring template_description;
 	};
 
+	/**
+	* @brief This namespace contains basic preistalled templates.
+	*/
 	namespace BuiltinProjectTemplates
 	{
+		/**
+		* @brief This template creates a basic C++ and GTKmm project.
+		*
+		* Uses the Makefile build system, but this can be changed.
+		*/
 		class BasicCPPTemplate : public ProjectTemplate
 		{
 			public:
